a54a485bee5c6ba7bffcede9e5865de67a05e401
ing.DaryCortez <daryjosecc2015@gmail.com>
2022-01-25 08:57:01 -0600

    Modificando proyecto VBCODE

---
+++ opcion.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+const mesesArray = [
+  "enero",
+  "febrero",
+  "marzo",
+  "abril",
+  "mayo",
+  "junio",
+  "julio",
+  "agosto",
+  "septiembre",
+  "octubre",
+  "noviembre",
+  "diciembre",
+]
+
+console.log(mesesArray[5])
